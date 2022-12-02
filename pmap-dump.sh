a5afac96fa83338742b2416a0e5901614e250a77
Nopius <Nopius@users.noreply.github.com>
2022-12-02 17:38:07 +1000

    Update pmap-dump.sh

--- pmap-dump.sh
+++ pmap-dump.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+# $1 - process name, dump all segments to files
+#!/bin/bash
 # $1 - process name, dump all segments, one-liner
-pgrep -f $1 | while read pid; do ( sudo pmap -x $pid | awk -vPID=$pid "BEGIN{ printf("pmap-dump -p " PID)};($5~/^r/){printf(" 0x" $1 " " $2)};END{printf("\n")}"); done | while read line; do sudo $line; done
+pgrep -f $1 | \
+while read pid
+do
+  (
+    sudo pmap -x $pid | \
+    awk -vPID=$pid 'BEGIN{ printf("pmap-dump -p " PID)};($5~/^r/){printf(" 0x" $1 " " $2)};END{printf("\n")}'
+  )
+done | while read line; do sudo $line; done
